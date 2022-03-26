32fd4210f632e0fbe332df6dde4f14754f2c701f
mihran-w <mihranw@gmail.com>
2022-03-26 18:47:34 +0430

    Add Project

---
+++ Encoded Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using Encoder;
+using System;
+
+namespace Encoded_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = true;
+
+            while (isActive == true)
+            {
+                Console.WriteLine("Enter Password For Hashing :");
+                string Password = Console.ReadLine();
+                string PasswordHash = Password.EncodeToMd5();
+                Console.WriteLine(PasswordHash);
+            }
+
+            //Console.ReadKey();
+
+        }
+    }
+}
